f5ad07aeedb2c9e99f9bc86235d204a316c3be69
Derek Santos <derek@santosapps.com>
2015-04-11 00:42:43 -0400

    implement test() and match() methods

--- src/censor.js
+++ src/censor.js
@@ -24,9 +24,43 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+/** @namespace */
 var Censor = (function() {
-	var locals = {
-		defaultWords : ['test'],
+	var utils = {
+		toString : Object.prototype.toString,
+		isString : function(obj) {
+			return utils.toString.call(obj) == '[object String]';
+		},
+		isArray : Array.isArray || function(obj) {
+    		return utils.toString.call(obj) === '[object Array]';
+  		},
+		unique : function(arr) {
+			var o = {}, i, l = arr.length, r = [];
+			for(i=0; i<l;i+=1){o[arr[i]] = arr[i];}
+			for(i in o){r.push(o[i]);}
+			return r;
+		},
+		each : function(arr, fn) {
+			var i, l = arr.length;
+			for(i = 0; i < l; i++) {
+				fn(arr[i], i);
+			}
+		},
+		eachChar : function(str, fn) {
+			var i, l = str.length;
+			for(i = 0; i < l; i++) {
+				fn(str.charAt(i), i);
+			}
+		},
+		warn : function(msg) {
+			if (console && console.warn) {
+				console.warn(msg);
+			}
+		}
+	};
+
+	var impl = {
+		words : [],
 		letterVariants : {
 			'a' : ['4','@'],
 			'b' : ['8'],
@@ -55,62 +89,95 @@ var Censor = (function() {
 			'y' : [],
 			'z' : ['7']
 		},
-		isString : function(obj) {
-			var toString = Object.prototype.toString;
-			return toString.call(obj) == '[object String]';
-		},
-		unique : function(arr) {
-			var o = {}, i, l = arr.length, r = [];
-			for(i=0; i<l;i+=1){o[arr[i]] = arr[i];}
-			for(i in o){r.push(o[i]);}
-			return r;
-		},
-		each : function(arr, fn) {
-			var i, l = arr.length;
-			for(i = 0; i < l; i++) {
-				fn(arr[i], i);
-			}
-		},
-		eachChar : function(str, fn) {
-			var i, l = str.length;
-			for(i = 0; i < l; i++) {
-				fn(str.charAt(i), i);
-			}
-		},
-		warn : function(msg) {
-			if (console && console.warn) {
-				console.warn(msg);
-			}
-		}
-	};
-
-	var exports = {
+		leftMatcher : '(^|\\s)',
+		rightMatcher : '(?=\\s|$)',
 		getVariants : function(word) {
 			var output,variants;
 			output = [];
-			if (word && locals.isString(word)) {
-				locals.eachChar(word, function(character) {
-					variants = locals.letterVariants[character];
+			if (word && utils.isString(word)) {
+				utils.eachChar(word, function(character) {
+					variants = impl.letterVariants[character];
 					if (variants && variants.length) {
-						locals.each(variants, function(variant) {
+						utils.each(variants, function(variant) {
 							output.push(word.replace(character, variant));
 						});
 					}
 				});
 				if (output.length) {
-					locals.each(output, function(item) {
-						output = output.concat(exports.getVariants(item));
+					utils.each(output, function(item) {
+						output = output.concat(impl.getVariants(item));
 					});
-					output = locals.unique(output);
+					output = utils.unique(output);
 				}
 			} else {
-				locals.warn("Censor.getVariants expected a string but got "+word);
+				utils.warn("Censor.getVariants expected a string but got " + word);
 			}
 			return output;
+		},
+		setWords : function(words) {
+			if (words && utils.isArray(words)) {
+				var arr = [];
+				utils.each(words, function(word) {
+					arr.push(word);
+					arr = arr.concat(impl.getVariants(word));
+				});
+				arr = utils.unique(words);
+				impl.words = arr;
+			} else {
+				utils.warn("Censor.setWords expected an array but got " + words.toString() + ". This call will be ignored.");
+			}
+		},
+		regex : function() {
+			var joinString = [')',impl.rightMatcher, '|', impl.leftMatcher,'('].join('');
+			var words = impl.words.join('sSPLITs');
+			words = words.replace(/([\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\{\}\|\[\]\\\;\:\'\"\,\.\/\<\>\?])/gi, '\\$1');
+			words = words.split('sSPLITs');
+			words = words.join(joinString);
+			var regexString = ['/',impl.leftMatcher,'(',words,')',impl.rightMatcher,'/'].join('');
+			console.log(regexString);
+			return new RegExp(regexString, 'gi');
 		}
 	};
 
-	return exports;
+	var api = {
+		/**
+		 * Instructs the library to use the specified array of strings as the words
+		 * to validate or match against. Note that Censor.getVariants will be called on 
+		 * each word.
+		 *
+		 * @param {array} words - The words desired for use when matching or validating.
+		 * @memberof Censor
+		 * @function
+		 */
+		setWords : impl.setWords,
+		/**
+		 * Returns a list of word variations spelt using symbols resembling it's characters.
+		 * For example the word <b>test</b> can be spelt as <b>t3s+</b> or <b>te$t</b>.
+		 * 
+		 * This is used to generate a comprehensive array of strings to validate against without
+		 * having to explicity provide every possible variation.
+		 *
+		 * @param {string} word - A single word to return variantions for.
+		 * @memberof Censor
+		 * @function
+		 */
+		getVariants : impl.getVariants,
+		/**
+		 * Returns true if the specified string contains any words that match
+		 * the list of words set with setWords. 
+		 * 
+		 * @param {string} str - The string to test against
+		 * @memberof Censor
+		 */
+		test : function(str) {
+			return impl.regex().test(str);
+		},
+		match : function(str) {
+			return str.match(impl.regex());
+		}
+	};
+
+	return api;
 }());
 
 //Check if were in a CommonJS environment
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,47 @@
 var assert = require("assert");
-var profanity = require('../src/profanity.js');
+var censor = require('../src/censor.js');
 
-describe('Profanity', function() {
+describe('Censor', function() {
 	describe('#getVariants()', function() {
 		var tests = [
 			{word: 'test', expected:["+est","t3st","te5t","te$t","te&t","+3st","+e5t","+e$t","+e&t","+es+","+35t","+3$t","+3&t","+3s+","+35+","+3$+","+3&+","+e5+","+e$+","+e&+","t35t","t3$t","t3&t"]}
 		];
 		tests.forEach(function(test) {
 			it('should return an array of variantions for the word '+test.word, function() {
-				var actual = profanity.getVariants(test.word);
+				var actual = censor.getVariants(test.word);
 				var expected = test.expected;
 				assert.equal(actual.length, expected.length);
 				//sort and join results to facilitate comparison
 				assert.equal(actual.sort().join(''), expected.sort().join(''));
+			});
+		});
+	});
+	describe('#test()', function() {
+		var goodStrings = [
+			'This should pass.',
+			'Also pass this sentence.',
+			'scone should pass',
+			'Scone should pass'
+		];
+		var badStrings = [
+			'one should fail',
+			'fail 0ne',
+			'fail 0n3',
+			'Should two fail.',
+			'+w0 should fail',
+			'one should one fail'
+		];
+		before(function() {
+			censor.setWords(['one','two','three']);
+		});
+		goodStrings.forEach(function(str) {
+			it('should return false for string: '+str, function() {
+				assert.equal(censor.test(str), false);
+			});
+		});
+		badStrings.forEach(function(str) {
+			it('should return true for string: '+str, function() {
+				assert.equal(censor.test(str), []);
 			});
 		});
 	});
